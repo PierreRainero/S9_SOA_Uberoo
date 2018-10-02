2757462ae3cd4287e50c47c042df00fcb859e88d
renaud <renaud.costa84@gmail.com>
2018-10-02 22:32:33 +0200

    create restaurant service

---
+++ restaurantService/src/test/java/fr/unice/polytech/si5/soa/a/restaurantservice/RestaurantserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.unice.polytech.si5.soa.a.restaurantservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
