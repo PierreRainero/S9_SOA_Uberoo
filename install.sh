31b90bd5b9fc6974057ca6d3aa9e479118660a59
Pierre Rainero <pierre.rainero@etu.unice.fr>
2018-10-06 20:34:08 +0200

    re-add healthy condition for slow PC (avoid restart loop for database), rename db container to explicit it's only for orderService

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "*******Building Docker image for the database*******"
-cd database
-./compile.sh
 echo "*******Building Docker image for the message bus*******"
-cd ../messageBus
+cd ./messageBus
 ./compile.sh
 echo "*******Building Docker image for the order service*******"
 cd ../orderService
 ./compile.sh
 echo "*******Installation complete*******"
+
 # echo "Press enter to close"
 # read
