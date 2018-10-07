207159641fc36a4a453af93519cbb872870ac6ff
renaudcosta <renaud.costa84@gmail.com>
2018-10-06 19:37:35 -0700

    Basic service

--- restaurantService/src/main/java/fr/unice/polytech/si5/soa/a/restaurantservice/RestaurantserviceApplication.java
+++ restaurantService/src/main/java/fr/unice/polytech/si5/soa/a/restaurantservice/RestaurantserviceApplication.java
@@ -1,9 +1,16 @@
 package fr.unice.polytech.si5.soa.a.restaurantservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+
+@EnableAutoConfiguration
+
+@ComponentScan
 public class RestaurantserviceApplication {
 
 	public static void main(String[] args) {
