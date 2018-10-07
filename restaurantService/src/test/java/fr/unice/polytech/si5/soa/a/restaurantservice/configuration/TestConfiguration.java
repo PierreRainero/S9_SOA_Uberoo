5621376972478efa564751000b0109e31064cef7
Pierre Rainero <pierre.rainero@etu.unice.fr>
2018-10-07 17:23:13 +0200

    add test to restaurantService

--- restaurantService/src/test/java/fr/unice/polytech/si5/soa/a/restaurantservice/configuration/TestConfiguration.java
+++ restaurantService/src/test/java/fr/unice/polytech/si5/soa/a/restaurantservice/configuration/TestConfiguration.java
@@ -1,37 +1,51 @@
 package fr.unice.polytech.si5.soa.a.restaurantservice.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+import org.springframework.web.client.RestTemplate;
+import fr.unice.polytech.si5.soa.a.restaurantservice.dao.IOrderDao;
 import fr.unice.polytech.si5.soa.a.restaurantservice.model.OrderToPrepare;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
+/**
+ * Class name	TestConfiguration
+ * Date			29/09/2018
+ * @author		PierreRainero
+ */
 @Configuration
 @PropertySource("classpath:db.properties")
 @EnableTransactionManagement
-// Components to used :
+// Components to used
 @ComponentScans(value = { 
 		@ComponentScan("fr.unice.polytech.si5.soa.a.restaurantservice.dao"),
 		@ComponentScan("fr.unice.polytech.si5.soa.a.restaurantservice.services")
 })
-public class ApplicationConfiguration {
+public class TestConfiguration {
 	@Autowired
 	private Environment env;
 
 	@Bean
 	public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(env.getProperty("db.driver"));
-		dataSource.setUrl(env.getProperty("db.url"));
-		dataSource.setUsername(env.getProperty("db.username"));
-		dataSource.setPassword(env.getProperty("db.password"));
+		dataSource.setDriverClassName(env.getProperty("hsqldb.driver"));
+		dataSource.setUrl(env.getProperty("hsqldb.url"));
+		dataSource.setUsername(env.getProperty("hsqldb.username"));
+		dataSource.setPassword(env.getProperty("hsqldb.password"));
 		return dataSource;
 	}
 
@@ -43,9 +57,10 @@ public class ApplicationConfiguration {
 		Properties props = new Properties();
 		props.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
 		props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-		props.put("hibernate.hbm2ddl.auto", env.getProperty("db.hbm2ddl.auto"));
-		props.put("hibernate.dialect", env.getProperty("db.dialect"));
+		props.put("hibernate.hbm2ddl.auto", env.getProperty("hsqldb.hbm2ddl.auto"));
+		props.put("hibernate.dialect", env.getProperty("hsqldb.dialect"));
 
+		// Entities
 		factoryBean.setHibernateProperties(props);
 		factoryBean.setAnnotatedClasses(OrderToPrepare.class);
 		return factoryBean;
@@ -56,5 +71,17 @@ public class ApplicationConfiguration {
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
 		transactionManager.setSessionFactory(getSessionFactory().getObject());
 		return transactionManager;
+	}
+	
+	@Qualifier("mock")
+	@Bean
+    public IOrderDao iOrderDao() {
+        return Mockito.mock(IOrderDao.class);
+    }
+	
+	@Qualifier("mock")
+	@Bean
+	public RestTemplate restTemplate() {
+	    return Mockito.mock(RestTemplate.class);
 	}
 }
