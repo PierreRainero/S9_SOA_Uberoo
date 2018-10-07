a9d029626645216b8926c342dc2588096e161063
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2018-10-07 14:31:35 +0200

    Added everything needed to create Docker images for coursierService and restaurantService

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ cd ./messageBus
 echo "*******Building Docker image for the order service*******"
 cd ../orderService
 ./compile.sh
+echo "*******Building Docker image for the restaurant service*******"
+cd ../restaurantService
+./compile.sh
+echo "*******Building Docker image for the coursier service*******"
+cd ../coursierService
+./compile.sh
 echo "*******Installation complete*******"
 
 # echo "Press enter to close"
