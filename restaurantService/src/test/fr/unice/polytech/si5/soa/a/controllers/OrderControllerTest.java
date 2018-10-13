48fa7fc0569b01e86870593111abbad17900b35b
renaudcosta <renaud.costa84@gmail.com>
2018-10-13 18:33:23 +0200

    POM dependencies updated

---
+++ restaurantService/src/test/fr/unice/polytech/si5/soa/a/controllers/OrderControllerTest.java
@@ -0,0 +1,20 @@
+package fr.unice.polytech.si5.soa.a.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void updateOrderState() {
+    }
+
+    @Test
+    void getDeliveriesToDo() {
+    }
+}
