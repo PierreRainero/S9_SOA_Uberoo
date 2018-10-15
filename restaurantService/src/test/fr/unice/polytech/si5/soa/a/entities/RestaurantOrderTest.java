39e24157810ea37423dc84c09cc8ee46ebd128b7
renaudcosta <renaud.costa84@gmail.com>
2018-10-15 16:05:35 +0200

    Test for toDTO in RestaurantOrder

---
+++ restaurantService/src/test/fr/unice/polytech/si5/soa/a/entities/RestaurantOrderTest.java
@@ -0,0 +1,31 @@
+package fr.unice.polytech.si5.soa.a.entities;
+
+import fr.unice.polytech.si5.soa.a.communication.RestaurantOrderDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantOrderTest {
+
+    private RestaurantOrder order;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        order = new RestaurantOrder();
+
+    }
+
+    @Test
+    void toDTO() {
+        List<String> meals = new ArrayList<>();
+        meals.add("Coca");
+        order.setMeals(meals);
+        order.setState(OrderState.TO_PREPARE);
+        order.setId(0);
+        assertEquals(order.toDTO(), new RestaurantOrderDTO(0, meals, OrderState.TO_PREPARE));
+    }
+}
