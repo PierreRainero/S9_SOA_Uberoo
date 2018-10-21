48def5ba5a4dc03fb56d441ae2e09c25b41f2507
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2018-10-21 10:48:36 +0200

    Databases are now only one to reduce RAM consumption

---
+++ database/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS uberoo_orderService;
+CREATE DATABASE IF NOT EXISTS uberoo_coursierService;
+CREATE DATABASE IF NOT EXISTS uberoo_restaurantService;
+GRANT ALL ON *.* TO 'root'@'%';
