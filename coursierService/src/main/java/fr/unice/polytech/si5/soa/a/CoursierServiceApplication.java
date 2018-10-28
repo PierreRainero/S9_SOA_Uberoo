032bcabdbc7046acfea1d6d33d4cdad3a6d3e392
Alexis Deslandes <alexis.deslandes@etu.unice.fr>
2018-10-28 10:45:21 +0100

    Add message listener for coursier and order service

--- coursierService/src/main/java/fr/unice/polytech/si5/soa/a/CoursierServiceApplication.java
+++ coursierService/src/main/java/fr/unice/polytech/si5/soa/a/CoursierServiceApplication.java
@@ -1,13 +1,19 @@
 package fr.unice.polytech.si5.soa.a;
 
+import fr.unice.polytech.si5.soa.a.message.MessageListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CoursierServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CoursierServiceApplication.class, args);
+	public static void main(String[] args) throws InterruptedException {
+		ConfigurableApplicationContext context = SpringApplication.run(CoursierServiceApplication.class, args);
+		MessageListener listener = context.getBean(MessageListener.class);
+		listener.getLatch().await(10, TimeUnit.SECONDS);
 	}
 }
