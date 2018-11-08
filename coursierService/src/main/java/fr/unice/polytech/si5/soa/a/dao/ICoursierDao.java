a9212da31638dea1516bfd83a30641ddc9809b2d
AlexisDeslandes <deslandes.alexis@live.fr>
2018-11-08 20:04:05 +0100

    Comments for coursier service - dao

--- coursierService/src/main/java/fr/unice/polytech/si5/soa/a/dao/ICoursierDao.java
+++ coursierService/src/main/java/fr/unice/polytech/si5/soa/a/dao/ICoursierDao.java
@@ -5,9 +5,38 @@ import fr.unice.polytech.si5.soa.a.entities.Coursier;
 import java.util.Optional;
 
 public interface ICoursierDao {
+    /**
+     * Add the following coursier to the system.
+     * @param coursier Coursier to add.
+     * @return The coursier added.
+     */
 	Coursier addCoursier(Coursier coursier);
+
+    /**
+     * Find the coursier by the following name.
+     * @param name Name of the coursier.
+     * @return The coursier which correspond.
+     */
 	Optional<Coursier> findCoursierByName(String name);
+
+    /**
+     * Find the coursier by the following id.
+     * @param idCoursier The id of the coursier.
+     * @return The coursier which match.
+     */
     Optional<Coursier> findCoursierById(Integer idCoursier);
+
+    /**
+     * Update the coursier in the system.
+     * @param coursier The new version of the coursier.
+     * @return The updated coursier.
+     */
     Coursier updateCoursier(Coursier coursier);
+
+    /**
+     * Get the coursier that is next to the following address.
+     * @param address The address we look for coursier
+     * @return The nearest coursier
+     */
     Optional<Coursier> getNearestCoursier(String address);
 }
--- coursierService/src/main/java/fr/unice/polytech/si5/soa/a/dao/IDeliveryDao.java
+++ coursierService/src/main/java/fr/unice/polytech/si5/soa/a/dao/IDeliveryDao.java
@@ -8,13 +8,46 @@ import fr.unice.polytech.si5.soa.a.entities.Delivery;
 /**
  * Class name	IDeliveryDao
  * Date			08/10/2018
- * @author		PierreRainero
+ *
+ * @author PierreRainero
  */
 public interface IDeliveryDao {
-	Delivery addDelivery(Delivery deliveryToAdd);
-	Delivery updateDelivery(Delivery deliveryToUpdate);
-	Optional<Delivery> findDeliveryById(int id);
-	List<Delivery> getDeliveriesToDo();
+    /**
+     * Add delivery to the service.
+     *
+     * @param deliveryToAdd The delivery to add
+     * @return The delivery added.
+     */
+    Delivery addDelivery(Delivery deliveryToAdd);
 
+    /**
+     * Update the following delivery.
+     *
+     * @param deliveryToUpdate Next state delivery
+     * @return Delivery which has been updated.
+     */
+    Delivery updateDelivery(Delivery deliveryToUpdate);
+
+    /**
+     * Find the delivery with the corresponding id.
+     *
+     * @param id Id of the delivery
+     * @return The corresponding delivery
+     */
+    Optional<Delivery> findDeliveryById(int id);
+
+    /**
+     * Get the deliveries which has not been done.
+     *
+     * @return The corresponding list of delivery
+     */
+    List<Delivery> getDeliveriesToDo();
+
+    /**
+     * Get the deliveries done by the coursier with the corresponding id.
+     *
+     * @param idCoursier Id of the coursier.
+     * @return The corresponding list of delivery.
+     */
     List<Delivery> getDeliveriesDoneBy(Integer idCoursier);
 }
